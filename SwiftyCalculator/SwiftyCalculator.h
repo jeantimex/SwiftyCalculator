eac04cc632c9bde02982f0d4202fd31cf8b5edda
jeantimex <jean.timex@gmail.com>
2019-02-21 22:52:32 -0800

    Initial Setup

---
+++ SwiftyCalculator/SwiftyCalculator.h
@@ -0,0 +1,18 @@
+//
+//  SwiftyCalculator.h
+//  SwiftyCalculator
+//
+//  Created by Yong Su on 2/21/19.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftyCalculator.
+FOUNDATION_EXPORT double SwiftyCalculatorVersionNumber;
+
+//! Project version string for SwiftyCalculator.
+FOUNDATION_EXPORT const unsigned char SwiftyCalculatorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftyCalculator/PublicHeader.h>
+
+
---
+++ SwiftyCalculatorExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftyCalculatorExample
+//
+//  Created by Yong Su on 2/21/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
