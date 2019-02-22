c5485128af94ba4215f548e0a2294097d71890e7
jeantimex <jean.timex@gmail.com>
2019-02-21 22:55:57 -0800

    Added Podfile

---
+++ Pods/Target Support Files/Pods-SwiftyCalculator/Pods-SwiftyCalculator-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SwiftyCalculator : NSObject
+@end
+@implementation PodsDummy_Pods_SwiftyCalculator
+@end
---
+++ Pods/Target Support Files/Pods-SwiftyCalculatorTests/Pods-SwiftyCalculatorTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SwiftyCalculatorTests : NSObject
+@end
+@implementation PodsDummy_Pods_SwiftyCalculatorTests
+@end
